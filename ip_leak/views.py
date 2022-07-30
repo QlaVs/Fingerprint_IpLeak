9c3e8ed069975561ae15a8cbaaa3dfe797b3d6e5
petrulya_2000@mail.ru <qLQUNashY8AucRk>
2022-07-30 15:00:15 +0300

    First commit

---
+++ ip_leak/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from ipware import get_client_ip
+import requests
+
+proxy_headers = ['CLIENT_IP', 'FORWARDED', 'FORWARDED_FOR',
+                 'FORWARDED_FOR_IP', 'VIA', 'X_FORWARDED',
+                 'X_FORWARDED_FOR', 'HTTP_CLIENT_IP', 'HTTP_FORWARDED',
+                 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED_FOR_IP', 'HTTP_PROXY_CONNECTION',
+                 'HTTP_VIA', 'HTTP_X_FORWARDED', 'HTTP_X_FORWARDED_FOR']
+
+
+# Create your views here.
+def index(request):
+    template = "index.html"
+    context = {}
+    proxy_headers_list = []
+
+    # Get IP
+    ip, is_routable = get_client_ip(request)
+    context["ip"] = ip
+
+    # Check proxy and headers
+    all_headers = dict(request.headers)
+    for pr_header in proxy_headers:
+        if pr_header in all_headers:
+            proxy_headers_list.append(pr_header)
+
+    if len(proxy_headers_list) > 0:
+        context["proxy_headers"] = True
+        context["proxy"] = True
+        context["proxy_headers_list"] = proxy_headers_list
+    else:
+        context["proxy_headers"] = False
+        context["proxy"] = False
+
+    # Check for VPN
+    context["VPN"] = False
+
+    # Check for TOR
+    raw_data = requests.get('https://check.torproject.org/exit-addresses')
+    data = list(raw_data.text.splitlines())
+    for tor_ip in data:
+        if "ExitAddress" in tor_ip:
+            tor_ip = tor_ip.split(" ")[1]
+            if ip == tor_ip:
+                context["TOR"] = True
+            else:
+                context["TOR"] = False
+
+    # Check for being on cite before
+    context["was_here"] = True
+
+    return render(request, template, context)
